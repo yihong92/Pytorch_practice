8f8fc4dfea5d2855122b12825a5dd9c1cd05c384
houg <CC8801235@gmail.com>
2026-05-27 16:31:36 +0800

    完成第00章節教學
    有關torch.tensor的使用

--- Learning pytorch/PyTorch_Fundamentals/practice.py
+++ Learning pytorch/PyTorch_Fundamentals/practice.py
@@ -263,3 +263,32 @@ print(f"Tensor D:\n{random_tensor_D}")
 print(f"Does Tensor C equal Tensor D? (anywhere)")
 print(random_tensor_C == random_tensor_D)
 
+# Check for GPU
+print(torch.version.cuda)
+print(torch.cuda.is_available())
+
+# Set device type
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+# Count number of devices
+print(torch.cuda.device_count())
+
+# Create tensor (default on CPU)
+tensor = torch.tensor([1, 2, 3])
+
+# Tensor not on GPU
+print(tensor, tensor.device)
+
+# Move tensor to GPU (if available)
+tensor_on_gpu = tensor.to(device)
+print(tensor_on_gpu)
+
+# If tensor is on GPU, can't transform it to NumPy (this will error)
+#tensor_on_gpu.numpy()
+
+# Instead, copy the tensor back to cpu
+tensor_back_on_cpu = tensor_on_gpu.cpu().numpy()
+print(tensor_back_on_cpu)
+print(tensor_on_gpu) # copy of the GPU tensor in CPU memory so the original tensor is still on GPU
+
