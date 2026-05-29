024e702c383a64efa1e841f81fec87ea3de1a25e
houg <CC8801235@gmail.com>
2026-05-29 15:30:56 +0800

    完成01教學
    須注意模型與tensor都必須用同一個device，還有numpy只能處理在CPU上的資料

--- Learning pytorch/PyTorch_Workflow_Fundamentals/Putting_it_all_together.py
+++ Learning pytorch/PyTorch_Workflow_Fundamentals/Putting_it_all_together.py
@@ -88,3 +88,114 @@ loss_fn = nn.L1Loss()
 # Create optimizer
 optimizer = torch.optim.SGD(params=model_1.parameters(), # optimize newly create model's parameters
                             lr=0.01)
+
+torch.manual_seed(42)
+
+# Set the number of epochs
+epochs = 1000
+
+# Put data on the available device
+# Without this, error will happen (not all model/data on device)
+X_train = X_train.to(device)
+X_test = X_test.to(device)
+y_train = y_train.to(device)
+y_test = y_test.to(device)
+
+for epoch in range(epochs):
+    ### Training
+    model_1.train() # train mode is on by default after construction
+
+    # 1. Forward pass
+    y_pred = model_1(X_train)
+
+    # 2. Calculate loss
+    loss = loss_fn(y_pred, y_train)
+
+    # 3. Zero grad optimizer
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    loss.backward()
+
+    # 5. Step the optimizer
+    optimizer.step()
+
+    ### Testing
+    model_1.eval() # put the model in evaluation mode for testing (inference)
+
+    # 1. Forward pass
+    with torch.inference_mode():
+        test_pred = model_1(X_test)
+
+        # 2. Calculate the loss
+        test_loss = loss_fn(test_pred , y_test)
+
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Train loss: {loss} | Testing loss: {test_loss}")
+
+# Find our model's learned parameters
+from pprint import pprint # pprint = pretty print, see: https://docs.python.org/3/library/pprint.html 
+print("The model learned the following values for weights and bias:")
+pprint(model_1.state_dict())
+print("\nAnd the original values for weights and bias are:")
+print(f"weights: {weight}, bias: {bias}")
+
+# Turn model into evaluation mode
+model_1.eval()
+
+# Make predictions on the test data
+with torch.inference_mode():
+    y_pred = model_1(X_test)
+print(y_pred)
+
+# plot_prediction(predictions=y_preds) # -> won't work... data not on CPU
+
+# Plot data on the CPU and plot it
+plot_predictions(predictions=y_pred.cpu())
+plt.show()
+
+from pathlib import Path
+
+# 1. Create models directory
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+# 2. Create model save path
+MODEL_NAME = "01_pytorch_workflow_model_1.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# 3. Save the model state dict
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_1.state_dict(), # only the state_dict() only saves the models learned parameters
+           f=MODEL_SAVE_PATH)
+
+# Instantiate a fresh instance of LinearRegressionModelV2
+loaded_model_1 = LinearRegressionModelV2()
+
+# Load model state dict
+loaded_model_1.load_state_dict(torch.load(MODEL_SAVE_PATH))
+
+# Put model to target device (if your data is on GPU, model will have to be on GPU to make predictions)
+loaded_model_1.to(device)
+
+print(f"Loaded model:\n{loaded_model_1}")
+print(f"Model on device:\n{next(loaded_model_1.parameters()).device}")
+
+# Evaluate loaded model
+loaded_model_1.eval()
+with torch.inference_mode():
+    loaded_model_1_preds = loaded_model_1(X_test)
+
+print(y_pred == loaded_model_1_preds)
+
+
+
+
+
+
+
+
+
+
+
+
